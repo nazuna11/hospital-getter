2bafa772ddaf34d982ba3a71e3c93c4cb0d548ba
nazuna <nazuna11develop@gmail.com>
2020-02-15 19:48:35 +0900

    [add] src

---
+++ src/hospital-getter.ts
@@ -0,0 +1,23 @@
+import { Client } from 'pg';
+
+export class HospitalGetter{
+  private client!: Client;
+
+  public async connect(host: string, user: string, password: string, database: string){
+    this.client = new Client({
+      host: host,
+      user: user,
+      password: password,
+      database: database
+    });
+    await this.client.connect();
+  }
+
+  public async query(query: string, parameters: any[] = []){
+    return (await this.client.query(query, parameters)).rows;
+  }
+
+  public async end(){
+    await this.client.end();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import {HospitalGetter} from './hospital-getter';
+require('dotenv').config();
+
+const hg = new HospitalGetter();
+
+let db_host: string = process.env.DB_HOST || '';
+let db_user: string = process.env.USER || '';
+let db_password: string = process.env.PASSWORD || '';
+let db_database: string = process.env.DATABASE || '';
+
+hg.connect(db_host, db_user, db_password, db_database);
+const query: string = 'SELECT * FROM '+process.env.SCHEMA+'.'+process.env.TABLE;
+const rows = hg.query(query);
+rows.then(function(resolve:any) {console.log(resolve)});
+/*
+var client = new Client({
+    user: process.env.USER,
+    host: process.env.DB_HOST,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+});
+client.connect();
+
+const query = {
+    text: 'SELECT * FROM '+process.env.SCHEMA+'.'+process.env.TABLE
+}
+
+client.query(query)
+  .then(res => console.log(res.rows))
+  .catch(e => console.error(e.stack))
+
+*/
