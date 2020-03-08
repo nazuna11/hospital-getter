02699a0d5787a485bc475bc447f333c5257ce790
nazuna <nazuna11develop@gmail.com>
2020-03-08 17:22:45 +0900

    [add] return hospitals

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {HospitalGetter} from './hospital-getter';
 require('dotenv').config();
 import {Hospital} from './hospital-getter';
+import http, {IncomingMessage, ServerResponse} from 'http'
 
 const hg = new HospitalGetter();
 
@@ -9,8 +10,18 @@ let dbUser: string = process.env.USER || '';
 let dbPassword: string = process.env.PASSWORD || '';
 let dbDatabase: string = process.env.DATABASE || '';
 
-hg.connect(dbHost, dbUser, dbPassword, dbDatabase);
-let nowlon = 139.3374233
-let nowlat = 35.7110031
-let rows = hg.getNearHospital(nowlon, nowlat, 600.0);
-rows.then(function(resolve:Hospital[]) {console.log(resolve[0])});
+try {
+  hg.connect(dbHost, dbUser, dbPassword, dbDatabase);
+}catch(e){
+  console.log("error");
+}
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  let nowlon = 139.3374233
+  let nowlat = 35.7110031
+  let rows = hg.getNearHospital(nowlon, nowlat, 600.0);
+  var res2 = res;
+  rows.then((result:Hospital[]) => {return res2.end(JSON.stringify(result))}, (error) => {return res2.end(JSON.stringify(error.message))})
+  // res2.end("ok")
+})
+
+server.listen(4000)
