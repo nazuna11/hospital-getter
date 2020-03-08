4dcf0fa9742a320bf465b92381f015cae4434552
nazuna <nazuna11develop@gmail.com>
2020-03-08 18:17:17 +0900

    [update] return value

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {HospitalGetter} from './hospital-getter';
 require('dotenv').config();
 import {Hospital} from './hospital-getter';
 import http, {IncomingMessage, ServerResponse} from 'http'
+import * as url from 'url'
 
 const hg = new HospitalGetter();
 
@@ -16,11 +17,36 @@ try {
   console.log("error");
 }
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  let nowlon = 139.3374233
-  let nowlat = 35.7110031
-  let rows = hg.getNearHospital(nowlon, nowlat, 600.0);
-  var res2 = res;
-  rows.then((result:Hospital[]) => {return res2.end(JSON.stringify(result))}, (error) => {return res2.end(JSON.stringify(error.message))})
+  if(req.method!="GET"){
+    res.statusCode = 400;
+    res.end('bad');
+  }else{
+    var nowlon = 139.3374233
+    var nowlat = 35.7110031
+    var default_radius = 600.0
+    var res2 = res;
+    var qstr:string;
+    if(typeof req.url == "string"){
+      qstr=req.url;
+      let param = url.parse(qstr, true).query;
+      var radius = 'radius' in param ? Number(param.radius) : default_radius
+      if('lon' in param && 'lat' in param){
+        let rows = hg.getNearHospital(Number(param.lon), Number(param.lat), radius);
+        res2.setHeader('Content-type', 'application/json');
+        rows.then((result:Hospital[]) => {
+          res2.statusCode = 200;
+          return res2.end(JSON.stringify(result), 'utf-8')
+        },
+        (error) => {
+          res2.statusCode = 400;
+          return res2.end(JSON.stringify(error.message))
+        })
+      }else{
+        res2.statusCode = 400;
+        return res2.end("no param")
+      }
+    }
+  }
   // res2.end("ok")
 })
 
